6bb17407054eed2e239377838ab467372b134cfd
pedromoraes <pedromoraes@e6ac747d-d54d-0410-a800-e1e48e7a7343>
2010-01-19 21:52:32 +0000

    hxmotion: missing files

---
+++ trunk/haxe/branches/hxMotion/src/jshelpers/FrameDispatcher.hx
@@ -0,0 +1,25 @@
+﻿/**
+ * ...
+ * @author pedromoraes@gmail.com
+ */
+
+package jshelpers;
+import haxe.Timer;
+import hxmotion.Types;
+
+class FrameDispatcher extends EventDispatcher
+{
+
+	public function new() : Void
+	{
+		super();
+		dispatch();
+	}
+	
+	private function dispatch() : Void
+	{
+		this.dispatchEvent( new Event( 'enterFrame' ) );
+		Timer.delay( dispatch, 25 );
+	}
+	
+}
