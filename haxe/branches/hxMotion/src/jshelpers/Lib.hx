4a6cbabe181197279ed628db9b613ccdccdc53fb
pedromoraes <pedromoraes@e6ac747d-d54d-0410-a800-e1e48e7a7343>
2010-01-19 21:52:32 +0000

    hxmotion: missing files

---
+++ haxe/branches/hxMotion/src/jshelpers/Lib.hx
@@ -0,0 +1,16 @@
+﻿/**
+ * ...
+ * @author pedromoraes@gmail.com
+ */
+
+package jshelpers;
+
+class Lib extends js.Lib
+{
+
+	public static function getTimer() : Int
+	{
+		return Std.int( Date.now().getTime() );
+	}
+	
+}
